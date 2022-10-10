3515d052c999773e4e89cf6d67daeacdcb09f090
Irwan Ngo <101211751@students.swinburne.edu.my>
2022-10-10 20:31:46 +0800

    Navbar and foundation for Dashboard
    Navbar is almost done, only requires current user. Groundwork for dashboard prepared.

---
+++ web_portal/dashboard.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset = "utf-8">
+	<meta name = "autor" content = "Irwan Ngo">
+	<link type="text/css" rel="stylesheet" href="style/dashboard.css">
+    <script src="https://kit.fontawesome.com/2ffaabbca0.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <title>ANPR - Dashboard</title>
+</head>
+
+<body>
+    <!--Sidebar starts here-->
+  <div class="navigation_bar">
+  <div class="logo"><img src="images/grab-logo.png"></div> 
+  <div class="navigation_links_container">
+
+  <div class="navigation_links"><a href="#"><i class="fa-solid fa-house"></i>Dashboard</a></div>
+  <div class="navigation_links"><a href="#"><i class="fa-solid fa-person-circle-plus"></i>Registration</a></div>
+  <div class="navigation_links drop_down_btn"><a href="#"><i class="fa-solid fa-clipboard-list"></i>Log<i class="fa-solid fa-angle-right"></i></a></div>
+    <div class="sub_menu">
+        <div class="navigation_links"><a href="#"></i>Entry Log</a></div>
+        <div class="navigation_links"><a href="#"></i>Exit Log</a></div>
+    </div>
+  
+  <div class="navigation_links"><a href="#"><i class="fa-solid fa-table"></i>Database</a></div>
+  <div class="navigation_links"><a href="#"><i class="fa-solid fa-user"></i>Profile</a></div>
+  <div class="navigation_links"><a href="#"><i class="fa-solid fa-arrow-right-from-bracket"></i>Logout</a></div>
+  
+</div>
+</div>
+</div>
+<script src="script/navbar.js"></script>
+<!--Sidebar ends here-->
+
+<div class="main">
+    <h1>Content</h1>
+</div>
+
+</body>
+</html>
