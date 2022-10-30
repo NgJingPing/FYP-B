e9f53bc4cd0b7a093908aec516fc7505cd4b4eaf
CancerSia <jefferysia7511@gmail.com>
2022-10-31 00:59:56 +0800

    Change Password, and minor css bug for login page

--- web_portal/Security/profile.php
+++ web_portal/Security/profile.php
@@ -12,6 +12,60 @@
 			header("Location: ../login.php");
 		}
 	}
+
+	$servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "anprdb";
+
+  $msgErr = "";
+
+	$conn = mysqli_connect($servername, $username, $password, $dbname); // Create DB connection object
+      if($conn->connect_error){
+          die("Connection Failed: " . $conn->connect_error);
+      }
+
+	$oldpassword = $newpassword = $renewpassword = $error_msg = $msg = $successmsg = "";
+
+	if(isset($_POST["submit"])) {
+		$oldpassword = $_POST["oldpassword"];
+		$newpassword = $_POST["newpassword"];
+		$renewpassword = $_POST["renewpassword"];
+		$email = $session_email;
+
+		$myquery = "SELECT password, role, isAdvanced, userID FROM users WHERE email = '$email'";
+
+		$sql = mysqli_query($conn, $myquery);
+		$oldpassword = hash("sha256", $oldpassword);
+		$dbpass = "";
+		while($row = mysqli_fetch_assoc($sql)) {
+			$dbpass = $row['password'];
+			$role = $row['role'];
+			$isAdvanced = $row['isAdvanced'];
+			$userid = $row['userID'];
+		}
+
+		if($oldpassword != "" & $newpassword != "" & $renewpassword != "") {
+			if ($oldpassword == $dbpass){
+				if ($newpassword == $renewpassword){
+          $newpassword = hash("sha256", $newpassword);
+          $myquery = "UPDATE users set password = ? WHERE userID = ?";
+          $stmt = $conn->prepare($myquery);
+          $stmt->bind_param("si", $newpassword, $userid);
+          $stmt->execute();
+          $conn->close();
+          $successmsg = "New Password is saved.";
+          $email = $newpassword = $role = $isAdvanced = $renewpassword = $oldpassword = $error_msg = $userid ="";
+          $_POST["oldpassword"] = $_POST["newpassword"] = $_POST["renewpassword"] = "";
+        } else {
+          $error_msg = "<p>New Password does not same</p>";
+        }
+			} else {
+				$msg = "<p>Incorrect password entered</p>";
+			}
+    }
+
+	}
 ?>
 
 <!DOCTYPE HTML>
@@ -35,6 +89,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Bungee+Hairline&display=swap" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="style/style.css">
+
 
     <style>
     .card {
@@ -62,7 +117,7 @@
       display: inline-block;
       padding: 8px;
       color: white;
-      background-color: #000;
+      background-color: #061C17;
       text-align: center;
       cursor: pointer;
       width: 60%;
@@ -79,16 +134,125 @@
       margin-right: auto;
       width: 240px;
     }
+
+
+
+		input[type=password] {
+		  width: 60%;
+		  padding: 12px 20px;
+		  margin: 8px 0;
+		  display: inline-block;
+		  border: 1px solid #ccc;
+		  box-sizing: border-box;
+		}
+
+		.cancelbtn {
+		  width: 60%;
+		  padding: 10px 18px;
+		  background-color: #f44336;
+		}
+
+		.imgcontainer {
+		  text-align: center;
+		  margin: 24px 0 12px 0;
+		  position: relative;
+		}
+
+		.container {
+		  padding: 16px;
+			background-color: #f2f2f2;
+		}
+
+		span.psw {
+		  float: right;
+		  padding-top: 16px;
+		}
+
+		.modal {
+		  display: none; /* Hidden by default */
+		  position: fixed; /* Stay in place */
+		  z-index: 1; /* Sit on top */
+		  left: 0;
+		  top: 0;
+		  width: 100%; /* Full width */
+		  height: 100%; /* Full height */
+		  overflow: auto; /* Enable scroll if needed */
+		  background-color: #061C17; /* Fallback color */
+		  padding-top: 60px;
+		}
+
+		.modal-content {
+		  background-color: #f2f2f2;
+		  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+		  border: 1px solid #888;
+		  width: 80%; /* Could be more or less, depending on screen size */
+		}
+
+		.close {
+		  position: absolute;
+		  right: 25px;
+		  top: 0;
+		  color: #000;
+		  font-size: 35px;
+		  font-weight: bold;
+		}
+
+		.close:hover,
+		.close:focus {
+		  color: red;
+		  cursor: pointer;
+		}
+
+		/* Add Zoom Animation */
+		.animate {
+		  -webkit-animation: animatezoom 0.6s;
+		  animation: animatezoom 0.6s
+		}
+
+		@-webkit-keyframes animatezoom {
+		  from {-webkit-transform: scale(0)}
+		  to {-webkit-transform: scale(1)}
+		}
+
+		@keyframes animatezoom {
+		  from {transform: scale(0)}
+		  to {transform: scale(1)}
+		}
+
+		/* Change styles for span and cancel button on extra small screens */
+		@media screen and (max-width: 300px) {
+		  span.psw {
+		     display: block;
+		     float: none;
+		  }
+		  .cancelbtn {
+		     width: 100%;
+		  }
+		}
+
+
     </style>
 </head>
 
 <body>
     <!--Sidebar starts here-->
-    <div class="navigation_bar">
-    <div class="logo_container">
-    <div class="logo"><span class="logo_initial">V</span><span>ISION</span></div>
-    <div class="logo_tail"><span>ANPR</span></div>
+  <div class="navigation_bar">
+  <div class="logo_container">
+  <div class="logo"><span class="logo_initial">V</span><span>ISION</span></div>
+  <div class="logo_tail"><span>ANPR</span></div>
+  </div>
+  <div class="navigation_links_container">
+
+  <div class="navigation_links"><a href="dashboard.php" ><i class="fa-solid fa-house"></i>Dashboard</a></div>
+  <div class="navigation_links"><a href="register_vehicle.php"><i class="fa-solid fa-person-circle-plus"></i>Registration</a></div>
+  <div class="navigation_links drop_down_btn"><a href="#"><i class="fa-solid fa-clipboard-list"></i>Log<i class="fa-solid fa-angle-right"></i></a></div>
+    <div class="sub_menu">
+        <div class="navigation_links"><a href="report.php"></i>Report</a></div>
+        <div class="navigation_links"><a href="entry_log.php"></i>Entry Log</a></div>
+        <div class="navigation_links"><a href="exit_log.php"></i>Exit Log</a></div>
+        <div class="navigation_links"><a href="denied_access.php"></i>Denial Log</a></div>
     </div>
+<<<<<<< Updated upstream
     <div class="navigation_links_container">
 
     <div class="navigation_links"><a href="dashboard.php"><i class="fa-solid fa-house"></i>Dashboard</a></div>
@@ -102,11 +266,17 @@
       <div class="navigation_links"><a href="view_vehicle.php"><i class="fa-solid fa-table"></i>Database</a></div>
     <div class="navigation_links"><a href="profile.php" class="active_page"><i class="fa-solid fa-user"></i>Profile</a></div>
     <div class="navigation_links"><a href="../login.php"><i class="fa-solid fa-arrow-right-from-bracket"></i>Logout</a></div>
+=======
+>>>>>>> Stashed changes
 
-  </div>
-  </div>
-  </div>
-  <script src="script/log.js"></script>
+  <div class="navigation_links"><a href="view_vehicle.php"><i class="fa-solid fa-table"></i>Database</a></div>
+  <div class="navigation_links"><a href="profile.php" class="active_page"><i class="fa-solid fa-user"></i>Profile</a></div>
+  <div class="navigation_links"><a href="../login.php"><i class="fa-solid fa-arrow-right-from-bracket"></i>Logout</a></div>
+
+</div>
+</div>
+</div>
+<script src="script/log.js"></script>
 <!--Sidebar ends here-->
 
 <?php
@@ -130,7 +300,6 @@
     $isAdvanced = $row['isAdvanced'];
   }
 ?>
-
 
 <div class="content-container">
 		<header>
@@ -156,9 +325,56 @@
       <br>
       <p>NAIM Holdings Berhad</p>
       <br>
-      <p><button>Change Password</button></p>
+      <p><button onclick="document.getElementById('id01').style.display='block'">Change Password</button><br>
+				<span class="error"><?php echo $successmsg;?></span></p>
+      <br>
+
+      <?php
+        if ($isAdvanced == 1){
+          echo "<p><a href='register_user.php'><button>Register New Admin/Security</button></a></p>";
+        }
+      ?>
     </div>
-	</div>
+</div>
+
+
+<div id="id01" class="modal">
+
+  <form class="modal-content animate" action="profile.php" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+    </div>
+
+    <div class="container">
+      <label for="oldpassword"><b>Enter Old Password</b></label><br>
+      <input type="password" placeholder="Enter Old Password" name="oldpassword" required><span class="error"> * <?php echo $msg;?></span></p><br>
+
+      <label for="newpassword"><b>Enter New Password</b></label><br>
+      <input type="password" placeholder="Enter New Password" name="newpassword" required><span class="error"> * </span></p><br>
+
+			<label for="renewpassword"><b>Re-Enter New Password</b></label><br>
+      <input type="password" placeholder="Re-Enter New Password" name="renewpassword" required><span class="error"> * <?php echo $error_msg;?></span></p><br>
+
+      <button type="submit" name = "submit">Submit</button>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+    </div>
+  </form>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
 
 <div class="waves"><p>&</p></div>
 </body>
