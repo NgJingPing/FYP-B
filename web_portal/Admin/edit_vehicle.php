55289fc8b8d187563118093a1e51af35eb2de5db
CancerSia <jefferysia7511@gmail.com>
2022-11-15 13:58:08 +0800

    Update alert msg box

--- web_portal/Admin/edit_vehicle.php
+++ web_portal/Admin/edit_vehicle.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 	$session_email = $ssession_type = "";
-	// Resume the session 
+	// Resume the session
 	session_start();
-	// If $_SESSION['email'] not set, force redirect to login page 
-	if (!isset($_SESSION['email']) && !isset($_SESSION['type'])) { 
+	// If $_SESSION['email'] not set, force redirect to login page
+	if (!isset($_SESSION['email']) && !isset($_SESSION['type'])) {
 		header("Location: ../login.php");
 	} else { // Otherwise, assign the values into $session_email & $ssession_type
 		$session_email = $_SESSION['email'];
@@ -12,7 +12,7 @@
 			header("Location: ../login.php");
 		}
 	}
-?> 
+?>
 
 <!DOCTYPE HTML>
 <html lang="en">
@@ -23,11 +23,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ANPR - Edit</title>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>  
-    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>            
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />  
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://kit.fontawesome.com/2ffaabbca0.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -35,17 +35,48 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Bungee+Hairline&display=swap" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="style/registration.css">
-    
-    
+
+		<style>
+			.alert {
+				font-size: 18px;
+				font-weight: bold;
+			  background-color: #FFFFFF;
+			  color: white;
+			  opacity: 1;
+			  transition: opacity 0.6s;
+				width: 95%;
+				margin-left: auto;
+	      margin-right: auto;
+			}
+
+			.alert.success {background-color: #4DAC62;}
+			.alert.error {background-color: #f44336;}
+			.alert.warning {background-color: #ff9800;}
+
+			.closebtn {
+			  margin-left: auto;
+			  color: white;
+			  font-weight: bold;
+			  float: right;
+			  font-size: 28px;
+			  line-height: 22px;
+			  cursor: pointer;
+			  transition: 0.3s;
+			}
+
+			.closebtn:hover {
+			  color: black;
+			}
+		</style>
 </head>
 
 <body>
 	<!--Sidebar starts here-->
 	<div class="navigation_bar">
-	<div class="logo_container"> 
+	<div class="logo_container">
 	<img src="../images/naim.png" class="naim_logo"></img>
-	<div class="logo"><span class="logo_initial">V</span><span>ISION</span></div> 
-	<div class="logo_tail"><span>ANPR</span></div> 
+	<div class="logo"><span class="logo_initial">V</span><span>ISION</span></div>
+	<div class="logo_tail"><span>ANPR</span></div>
 	</div>
 	<div class="navigation_links_container">
 
@@ -58,11 +89,11 @@
 			<div class="navigation_links"><a href="exit_log.php"></i>Exit Log</a></div>
 			<div class="navigation_links"><a href="denied_access.php"></i>Denial Log</a></div>
 		</div>
-  
+
 	<div class="navigation_links"><a href="view_vehicle.php" class="active_page"><i class="fa-solid fa-table"></i>Database</a></div>
 	<div class="navigation_links"><a href="profile.php"><i class="fa-solid fa-user"></i>Profile</a></div>
 	<div class="navigation_links"><a href="../login.php"><i class="fa-solid fa-arrow-right-from-bracket"></i>Logout</a></div>
-  
+
 	</div>
 	</div>
 	</div>
@@ -117,7 +148,7 @@
 
 			if(empty($_POST["plateNumber"])) {
 				$plateNumberErr = "License Plate Number is required";
-			} elseif (strlen($_POST["plateNumber"]) > 15 ){ 
+			} elseif (strlen($_POST["plateNumber"]) > 15 ){
 				$plateNumberErr = "License plate number should not exceed 15 characters";
 			} else {
 				$plateNumber = test_input($_POST["plateNumber"]);
@@ -127,7 +158,7 @@
 
 			if(empty($_POST["tenantName"])) {
 				$tenantNameErr = "Tenant name is required";
-			} elseif (strlen($_POST["tenantName"]) > 50 ){ 
+			} elseif (strlen($_POST["tenantName"]) > 50 ){
 				$tenantNameErr = "Tenant name should not exceed 50 characters";
 			} else {
 				$tenantName = test_input($_POST["tenantName"]);
@@ -136,9 +167,9 @@
 
 			if(empty($_POST["contactNumber"])) {
 				$contactNumberErr = "Contact Number is required";
-			} elseif (strlen($_POST["contactNumber"]) > 10 ){ 
+			} elseif (strlen($_POST["contactNumber"]) > 10 ){
 				$contactNumberErr = "Contact number should not exceed 10 characters";
-			} elseif (!preg_match('/^[0-9]*$/',$_POST["contactNumber"])){ 
+			} elseif (!preg_match('/^[0-9]*$/',$_POST["contactNumber"])){
 				$contactNumberErr = "Contact number should only contain numbers";
 			} else {
 				$contactNumber = test_input($_POST["contactNumber"]);
@@ -147,7 +178,7 @@
 
 			if(empty($_POST["brand"])) {
 				$brandErr = "Brand is required";
-			} elseif (strlen($_POST["brand"]) > 20 ){ 
+			} elseif (strlen($_POST["brand"]) > 20 ){
 				$brandErr = "Brand should not exceed 20 characters";
 			} else {
 				$brand = test_input($_POST["brand"]);
@@ -156,7 +187,7 @@
 
 			if(empty($_POST["model"])) {
 				$modelErr = "Model is required";
-			} elseif (strlen($_POST["model"]) > 30 ){ 
+			} elseif (strlen($_POST["model"]) > 30 ){
 				$modelErr = "Model should not exceed 30 characters";
 			} else {
 				$model = test_input($_POST["model"]);
@@ -165,18 +196,18 @@
 
 			if(empty($_POST["color"])) {
 				$colorErr = "Colour is required";
-			} elseif (strlen($_POST["color"]) > 20 ){ 
+			} elseif (strlen($_POST["color"]) > 20 ){
 				$colorErr = "Colour should not exceed 20 characters";
 			} else {
 				$color = test_input($_POST["color"]);
 				$color = mysqli_escape_string($conn, $color);
 			}
 
-			$check = isset($_POST['active']) ? "checked" : "unchecked"; 
-			if($check == "checked") { 
+			$check = isset($_POST['active']) ? "checked" : "unchecked";
+			if($check == "checked") {
 				$active = TRUE;
-			} else { 
-				$active = FALSE; 
+			} else {
+				$active = FALSE;
 			}
 
 			if($plateNumber != "" && $tenantName != "" && $contactNumber !="" && $brand != "" && $model != "" && $color != "")
@@ -234,6 +265,15 @@
 		<h1>Edit</h1>
 	</header>
 
+	<?php
+		if ($msg != ""){
+			echo '<div class="alert success">
+							<span class="closebtn">&times;</span>
+							' . $msg . '
+						</div>';
+		}
+	?>
+
 	<section>
 		 <form method="post" action="">
 			<div class="com_con">
@@ -244,7 +284,7 @@
 					</div>
 					<div class="form_container">
 					<label>License Plate Number</label><input type="text" class="form_control" name="plateNumber" value="<?php echo isset($_POST["plateNumber"]) ? $_POST["plateNumber"] : ''; ?>">
-					</div>	
+					</div>
 					</div>
 					<div class="form_group">
 					<div class="form_container">
@@ -252,7 +292,7 @@
 					</div>
 					<div class="form_container">
 					<label>Contact Number</label><span class="error"> * <?php echo $contactNumberErr;?></span><input type="text" class="form_control" name="contactNumber" value="<?php echo isset($_POST["contactNumber"]) ? $_POST["contactNumber"] : ''; ?>">
-					</div>	
+					</div>
 					</div>
 					<div class="form_group">
 					<div class="form_container">
@@ -267,7 +307,7 @@
 					<label>Colour</label><span class="error"> * <?php echo $colorErr;?></span><input type="text" name="color" class="form_control" value="<?php echo isset($_POST["color"]) ? $_POST["color"] : ''; ?>">
 					</div>
 					<div class="form_container">
-					<label>Active</label> 
+					<label>Active</label>
 					<label class="switch">
 						<?php
 							if($checked == "checked") {
@@ -290,11 +330,24 @@
 					</div>
 					</div>
 				</fieldset>
-				 <p class="message"><span class="successMsg"><?php echo $msg;?></span><p>
 			</div>
 		 </form>
 	</section>
 	</div>
+
+	<script>
+
+	var close = document.getElementsByClassName("closebtn");
+	var i;
+
+	for (i = 0; i < close.length; i++) {
+	  close[i].onclick = function(){
+	    var div = this.parentElement;
+	    div.style.opacity = "0";
+	    setTimeout(function(){ div.style.display = "none"; }, 600);
+	  }
+	}
+	</script>
 	<div class="waves"></div>
 </body>
 </html>
