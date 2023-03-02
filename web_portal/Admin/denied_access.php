39d9c8340facc3798ecb5e40665aceaab6fcc937
Irwan Ngo <101211751@students.swinburne.edu.my>
2023-03-02 23:04:19 +0800

    Fix PDF export functionality

--- web_portal/Admin/denied_access.php
+++ web_portal/Admin/denied_access.php
@@ -40,6 +40,7 @@
     <script src="https://cdn.datatables.net/buttons/2.3.5/js/buttons.html5.min.js"></script> 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script> 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
     <!-- ENDS HERE -->
 
     <!-- Fonts CDN -->
